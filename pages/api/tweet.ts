56fd7d7e46cb04c69d352ec8d02b7ba44f823e55
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-01-20 14:41:08 +0530

    feat: send all data that is needed when requesting api route

--- pages/api/tweet.ts
+++ pages/api/tweet.ts
@@ -2,6 +2,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const TWITTER_TWEET_API_URL = "https://api.twitter.com/2/tweets/";
+const TWITTER_API_MORE_PARAMS =
+  "?expansions=author_id,referenced_tweets.id,in_reply_to_user_id,attachments.media_keys,entities.mentions.username,referenced_tweets.id.author_id&tweet.fields=created_at,text,in_reply_to_user_id,referenced_tweets,attachments,public_metrics&user.fields=name,username,verified,profile_image_url&media.fields=media_key,duration_ms,height,preview_image_url,type,url,width,public_metrics,alt_text";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
@@ -9,10 +11,14 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     console.log(tweetUrl);
     const tweetId = tweetUrl.split("/")[5];
     console.log(tweetId);
-    const twitterRes = await fetch(TWITTER_TWEET_API_URL + tweetId, {
+    const URL = TWITTER_TWEET_API_URL + tweetId + TWITTER_API_MORE_PARAMS;
+    console.log(URL);
+    const twitterRes = await fetch(URL, {
       headers: {
         Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
       },
+      method: "GET",
+      redirect: "follow",
     });
     const twitterData = await twitterRes.json();
     console.log(twitterData);

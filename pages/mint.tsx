32ef08267b21900dfe11cbb82e7c624a73cb9e2f
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-01-20 14:55:40 +0530

    style(mint): added a spinner when submitting tweet url

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import { ChangeEvent, useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Spinner } from "../components/Icons";
 
 const MintPage: NextPage = () => {
   const [tweetData, setTweetData] = useState();
@@ -12,13 +13,16 @@ const MintPage: NextPage = () => {
           setTweetData(
             await (await fetch(`api/tweet?tweetUrl${values.link}`)).json()
           );
+          setSubmitting(false);
         }}
       >
         {({ isSubmitting }) => (
           <Form>
             <Field type="text" name="link" />
             <ErrorMessage name="link" />
-            <button type="submit">Submit</button>
+            <button type="submit" className="text-white bg-black">
+              {isSubmitting ? <Spinner /> : "Submit"}
+            </button>
           </Form>
         )}
       </Formik>

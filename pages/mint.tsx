3cfc41bb86a96cf3835362006b779d6b63651401
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-01-21 15:30:16 +0530

    style(mint): styled tweet url form

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -5,11 +5,12 @@ import { format, formatISO } from "date-fns";
 import { Like, Reply, Retweet, Spinner } from "../components/Icons";
 import ITweetObject from "../types/TweetData";
 import Tweet from "../components/Tweet";
+import { HiArrowNarrowRight } from "react-icons/hi";
 
 const MintPage: NextPage = () => {
   const [tweetData, setTweetData] = useState<ITweetObject>();
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center">
       <Formik
         initialValues={{ link: "" }}
         onSubmit={async (values, { setSubmitting }) => {
@@ -23,11 +24,20 @@ const MintPage: NextPage = () => {
         }}
       >
         {({ isSubmitting }) => (
-          <Form>
-            <Field type="text" name="link" />
+          <Form className="flex items-center px-4 py-2 mt-8 transition duration-200 border-2 border-gray-500 shadow-lg px-w rounded-xl bg-secondary/60 backdrop-filter backdrop-blur-3x focus-within:border-accent hover:border-gray-600 focus-within:hover:border-accent">
+            <Field
+              type="text"
+              name="link"
+              placeholder="Tweet URL"
+              className="px-4 py-2 text-xl text-gray-300 placeholder-gray-500 bg-transparent border-none focus:outline-none focus:ring-0"
+            />
             <ErrorMessage name="link" />
-            <button type="submit" className="text-white bg-black">
-              {isSubmitting ? <Spinner /> : "Submit"}
+            <button type="submit" className="w-6 h-6 ml-2 text-gray-300">
+              {isSubmitting ? (
+                <Spinner />
+              ) : (
+                <HiArrowNarrowRight className="w-6 h-6 hover:opacity-80" />
+              )}
             </button>
           </Form>
         )}

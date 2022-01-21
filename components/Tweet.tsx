a657f43c005bac555e77e154a38722a0d9e426fa
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-01-21 15:55:50 +0530

    style(tweet): added some spacing in the header section of the tweet

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -30,9 +30,9 @@ const Tweet = ({ tweetData, tweetIncludes }: TweetProps): JSX.Element => {
   console.log(tweetIncludes);
 
   return (
-    <div className="flex flex-col p-8 text-gray-100 w-[40rem] rounded-2xl h-auto bg-secondary">
+    <div className="flex flex-col p-8 text-white w-[40rem] rounded-2xl bg-secondary">
       <div className="flex flex-col items-start justify-start">
-        <div className="flex space-x-2 col">
+        <div className="flex col">
           <img
             src={tweetIncludes.users[0].profile_image_url}
             alt={`{Profile picture for ${tweetIncludes.users[0].username}`}
@@ -40,9 +40,13 @@ const Tweet = ({ tweetData, tweetIncludes }: TweetProps): JSX.Element => {
           />
           <div className="flex flex-col justify-start ml-4">
             <p>{tweetIncludes.users[0].name}</p>
-            <p>@{tweetIncludes.users[0].username}</p>
+            <p className="mt-1 text-gray-300">
+              @{tweetIncludes.users[0].username}
+            </p>
           </div>
-          <p>{format(new Date(tweetData.created_at), "PPP")}</p>
+          <p className="ml-4 text-gray-300">
+            {format(new Date(tweetData.created_at), "PPP")}
+          </p>
         </div>
         <p>{tweetData.text}</p>
         <div className="flex flex-row space-x-4">

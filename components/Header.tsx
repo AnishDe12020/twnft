ac5e12658ff1627fae6ae790d22e2415c46459d1
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-01-30 13:08:53 +0530

    stlye(header): add logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import useUser from "../hooks/useUser";
+import { Logo } from "./Icons";
 
 const Header = (): JSX.Element => {
   const { signIn, user } = useUser();
@@ -8,31 +9,34 @@ const Header = (): JSX.Element => {
   console.log(user);
 
   return (
-    <nav className="flex items-center justify-end px-8 py-4 bg-gray-900">
-      <Link href="/mint" passHref>
-        <a className="relative z-10 px-4 py-2 mr-8 text-xl font-bold text-white rounded-lg bg-gradient-to-tr from-pink-700 to-blue-700 before:absolute before:inset-0 before:bg-gradient-to-bl before:from-pink before:opacity-0 before:-z-10 before:transition before:duration-500 before:hover:opacity-100 before:rounded-lg">
-          Mint NFT
-        </a>
-      </Link>
-      {user ? (
-        <div className="flex items-center justify-between p-2 text-white bg-gray-800 rounded-full">
-          <p className="ml-2 mr-4">{user.displayName}</p>
-          <img
-            src={user.providerData[0].photoURL as string}
-            alt="Profile Picture"
-            className="rounded-full"
-            width={48}
-            height={48}
-          />
-        </div>
-      ) : (
-        <button
-          className="p-2 text-white bg-blue-500 rounded-full"
-          onClick={signIn}
-        >
-          Sign In with Twitter
-        </button>
-      )}
+    <nav className="flex items-center justify-between px-8 py-4 bg-gray-900">
+      <Logo className="w-20 h-20" />
+      <div className="flex items-center">
+        <Link href="/mint" passHref>
+          <a className="relative z-10 px-4 py-2 mr-8 text-xl font-bold text-white rounded-lg bg-gradient-to-tr from-pink-700 to-blue-700 before:absolute before:inset-0 before:bg-gradient-to-bl before:from-pink before:opacity-0 before:-z-10 before:transition before:duration-500 before:hover:opacity-100 before:rounded-lg">
+            Mint NFT
+          </a>
+        </Link>
+        {user ? (
+          <div className="flex items-center justify-between p-2 text-white bg-gray-800 rounded-full">
+            <p className="ml-2 mr-4">{user.displayName}</p>
+            <img
+              src={user.providerData[0].photoURL as string}
+              alt="Profile Picture"
+              className="rounded-full"
+              width={48}
+              height={48}
+            />
+          </div>
+        ) : (
+          <button
+            className="p-2 text-white bg-blue-500 rounded-full"
+            onClick={signIn}
+          >
+            Sign In with Twitter
+          </button>
+        )}
+      </div>
     </nav>
   );
 };

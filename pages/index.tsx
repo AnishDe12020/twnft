030259e782f19e8234ab7a43a848769885fd82a5
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-01-30 21:56:41 +0530

    style(index): made it responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,10 @@ import { TiStar } from "react-icons/ti";
 const Home: NextPage = () => {
   return (
     <>
-      <Header />
-      <div className="flex flex-col justify-center mt-8 ml-16">
+      {/* <Header /> */}
+      <div className="flex flex-col items-center justify-center mx-8 mb-16 md:mx-12 lg:mx-16">
         <Logo className="self-center w-48 h-48 mb-8" />
-        <h1 className="text-5xl text-center text-white">
+        <h1 className="text-3xl text-center text-white md:text-4xl lg:text-5xl">
           Mint your tweets as{" "}
           <span className="font-bold text-transparent bg-clip-text bg-gradient-to-tr from-pink-400 to-blue-400">
             NFTs
@@ -20,7 +20,7 @@ const Home: NextPage = () => {
         </h1>
         <div className="flex justify-center mt-16 space-x-4">
           <a
-            className="text-white bg-[#2882e0] px-4 py-2 rounded-xl text-xl w-fit hover:opacity-60"
+            className="text-white bg-[#2882e0] px-4 py-2 rounded-xl text-md md:text-lg text-center lg:text-xl w-fit hover:opacity-60"
             target="_blank"
             rel="noopener noreferrer"
             href="https://testnets.opensea.io/collection/twnft"
@@ -28,13 +28,13 @@ const Home: NextPage = () => {
             See our collection on OpenSea
           </a>
           <Link href="/mint" passHref>
-            <a className="relative z-10 px-3 py-2 mr-8 text-xl text-white rounded-lg text-md bg-gradient-to-tr from-pink-700 to-blue-700 before:absolute before:inset-0 before:bg-gradient-to-bl before:from-pink before:opacity-0 before:-z-10 before:transition before:duration-500 before:hover:opacity-100 before:rounded-lg">
+            <a className="relative z-10 px-3 py-2 mr-8 text-center text-white rounded-lg text-md md:text-lg lg:text-xl bg-gradient-to-tr from-pink-700 to-blue-700 before:absolute before:inset-0 before:bg-gradient-to-bl before:from-pink before:opacity-0 before:-z-10 before:transition before:duration-500 before:hover:opacity-100 before:rounded-lg">
               Mint your tweet as an NFT
             </a>
           </Link>
         </div>
 
-        <div className="flex justify-around mt-16 space-x-4 text-white">
+        <div className="flex flex-col items-center justify-center mt-16 space-y-4 text-white md:space-x-8 lg:space-x-16 md:space-y-0 md:justify-around md:flex-row">
           <div className="flex flex-col items-center justify-center w-64">
             <MdDashboardCustomize className="w-12 h-12 mb-2" />
             <h3 className="text-lg font-semibold">Customizable tweet image</h3>

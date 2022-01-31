3551ab809b0b4cdaade26c6eaa77aabb3b252afb
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-01-31 12:25:42 +0530

    refactor(document): fix a typo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ import Document, {
 } from "next/document";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: Documen) {
+  static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
